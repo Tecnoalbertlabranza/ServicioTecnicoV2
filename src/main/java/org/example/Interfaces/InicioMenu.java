dae37b58520735e5d08203a1a989c23d08e9837f
Tecnoalbertlabranza <basty.wenckhansbello@gmail.com>
2024-11-05 21:36:57 -0300

    PequeñoCambio

--- src/main/java/org/example/Interfaces/InicioMenu.java
+++ src/main/java/org/example/Interfaces/InicioMenu.java
@@ -26,10 +26,16 @@ public class InicioMenu extends javax.swing.JPanel {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jLabel1 = new javax.swing.JLabel();
+
         setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jLabel1.setText("Aqui se mostrara el inicio");
+        add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(240, 210, -1, -1));
     }// </editor-fold>//GEN-END:initComponents
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel jLabel1;
     // End of variables declaration//GEN-END:variables
 }
