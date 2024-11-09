bdcc35389ef8e5f120b49dfd34cda4e2111f2e9b
rz-eizy <duartealessandroxd@gmail.com>
2024-11-09 17:01:16 -0300

    chore: Método calculo Iva en clase venta
    Se implementaron ciertos cambios tales como, una nueva varibale, ademas de la creacion del metodo calcular iva. Por otro lado se corrigieron los errores en los nombres de las variables, para que sigan las buenas practicas

--- src/main/java/org/example/Venta.java
+++ src/main/java/org/example/Venta.java
@@ -1,11 +1,12 @@
 package org.example;
 public class Venta {
 
-	private String FechaVenta;
-	private double Total;
+	private String fechaVenta;
+	private double total;
+	private int iva;
 
 	public void CalcularIva() {
-
+		iva = (int)(total * 0.16);
 	}
 
 	public void CalcularTotal() {
