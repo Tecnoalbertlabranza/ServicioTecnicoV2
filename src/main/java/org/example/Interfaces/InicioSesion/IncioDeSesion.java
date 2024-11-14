48e77ee9f4b515fdbe89bf50d5429e46d2679352
Tecnoalbertlabranza <basty.wenckhansbello@gmail.com>
2024-11-13 23:16:58 -0300

    Arreglo error de eliminacion
    pequeño error arreglado

--- src/main/java/org/example/Interfaces/InicioSesion/IncioDeSesion.java
+++ src/main/java/org/example/Interfaces/InicioSesion/IncioDeSesion.java
@@ -3,24 +3,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
 package org.example.Interfaces.InicioSesion;
-<<<<<<< HEAD
-import org.example.Main;
-import org.example.Servicio;
-import org.example.ServicioTecnico;
-import org.example.firebase;
-import org.netbeans.lib.awtextra.AbsoluteConstraints;
-import org.netbeans.lib.awtextra.AbsoluteLayout;
-
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
-public class IncioDeSesion extends JFrame {
-    ServicioTecnico s1 = new ServicioTecnico();
-=======
 
 /**
  *
@@ -31,11 +13,7 @@ public class IncioDeSesion extends javax.swing.JFrame {
     /**
      * Creates new form IncioDeSesion
      */
->>>>>>> parent of e8e0acb (feat: avance implementacion de firebase)
     public IncioDeSesion() {
-        firebase f1 = new firebase();
-        f1.inicializarconexion();
-        s1.RegistrarCliente("juanito","perez","labranza");
         initComponents();
     }
 
@@ -48,99 +26,86 @@ public class IncioDeSesion extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        Background = new JPanel();
-        jLabel1 = new JLabel();
-        jLabel2 = new JLabel();
-        jLabel3 = new JLabel();
-        CasillaContraseña = new JTextField();
-        jLabel4 = new JLabel();
-        CasillaNombreUsuario = new JTextField();
-        BotonIniciarSesion = new JButton();
+        Background = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        TextoContraseña = new javax.swing.JTextField();
+        jLabel4 = new javax.swing.JLabel();
+        TextoUsuario = new javax.swing.JTextField();
+        jButton1 = new javax.swing.JButton();
 
-        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        Background.setLayout(new AbsoluteLayout());
+        Background.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        jLabel1.setFont(new Font("Segoe UI", 0, 18)); // NOI18N
+        jLabel1.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
         jLabel1.setText("Logo");
-        Background.add(jLabel1, new AbsoluteConstraints(110, 60, 90, 50));
+        Background.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(110, 60, 90, 50));
 
-        jLabel2.setFont(new Font("Segoe UI Black", 0, 18)); // NOI18N
+        jLabel2.setFont(new java.awt.Font("Segoe UI Black", 0, 18)); // NOI18N
         jLabel2.setText("Iniciar Sesion");
-        Background.add(jLabel2, new AbsoluteConstraints(110, 140, 140, 40));
+        Background.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(110, 140, 140, 40));
 
-        jLabel3.setFont(new Font("Segoe UI", 0, 14)); // NOI18N
+        jLabel3.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
         jLabel3.setText("Contraseña");
-        Background.add(jLabel3, new AbsoluteConstraints(110, 280, 80, 20));
+        Background.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(110, 280, 80, 20));
 
-        CasillaContraseña.setBorder(null);
-        CasillaContraseña.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent evt) {
-                CasillaContraseñaActionPerformed(evt);
+        TextoContraseña.setForeground(new java.awt.Color(204, 204, 204));
+        TextoContraseña.setText("Ingrese su Contraseña");
+        TextoContraseña.setBorder(null);
+        TextoContraseña.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                TextoContraseñaActionPerformed(evt);
             }
         });
-        Background.add(CasillaContraseña, new AbsoluteConstraints(110, 320, 380, 30));
+        Background.add(TextoContraseña, new org.netbeans.lib.awtextra.AbsoluteConstraints(110, 320, 380, 30));
 
-        jLabel4.setFont(new Font("Segoe UI", 0, 14)); // NOI18N
-        jLabel4.setText("Ingrese su Nombre");
-        Background.add(jLabel4, new AbsoluteConstraints(110, 190, 190, 30));
+        jLabel4.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
+        jLabel4.setText("Usuario");
+        Background.add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(110, 190, 50, 20));
 
-        CasillaNombreUsuario.setBorder(null);
-        CasillaNombreUsuario.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent evt) {
-                CasillaNombreUsuarioActionPerformed(evt);
+        TextoUsuario.setText("Ingrese su nombre de usuario");
+        TextoUsuario.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                TextoUsuarioActionPerformed(evt);
             }
         });
-        Background.add(CasillaNombreUsuario, new AbsoluteConstraints(110, 220, 380, 30));
+        Background.add(TextoUsuario, new org.netbeans.lib.awtextra.AbsoluteConstraints(110, 220, 380, 30));
 
-        BotonIniciarSesion.setText("Iniciar Sesion");
-        BotonIniciarSesion.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent evt) {
-                BotonIniciarSesionActionPerformed(evt);
+        jButton1.setText("jButton1");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
             }
         });
-        Background.add(BotonIniciarSesion, new AbsoluteConstraints(240, 400, 150, 60));
+        Background.add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(190, 410, 150, 60));
 
-        GroupLayout layout = new GroupLayout(getContentPane());
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
-            layout.createParallelGroup(GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addComponent(Background, GroupLayout.PREFERRED_SIZE, 841, GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 0, Short.MAX_VALUE))
+                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addComponent(Background, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
-            layout.createParallelGroup(GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addComponent(Background, GroupLayout.PREFERRED_SIZE, 538, GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 0, Short.MAX_VALUE))
+                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addComponent(Background, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         pack();
-<<<<<<< HEAD
-    }
-    private void CasillaContraseñaActionPerformed(ActionEvent evt) {
-
-    }
-    private void CasillaNombreUsuarioActionPerformed(ActionEvent evt) {
-
-    }
-    private void BotonIniciarSesionActionPerformed(ActionEvent evt) {
-=======
     }// </editor-fold>//GEN-END:initComponents
 
-    private void CasillaContraseñaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_CasillaContraseñaActionPerformed
+    private void TextoContraseñaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_TextoContraseñaActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_CasillaContraseñaActionPerformed
+    }//GEN-LAST:event_TextoContraseñaActionPerformed
 
-    private void CasillaNombreUsuarioActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_CasillaNombreUsuarioActionPerformed
+    private void TextoUsuarioActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_TextoUsuarioActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_CasillaNombreUsuarioActionPerformed
->>>>>>> parent of e8e0acb (feat: avance implementacion de firebase)
+    }//GEN-LAST:event_TextoUsuarioActionPerformed
 
-    private void BotonIniciarSesionActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BotonIniciarSesionActionPerformed
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_BotonIniciarSesionActionPerformed
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -149,58 +114,42 @@ public class IncioDeSesion extends javax.swing.JFrame {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html
          */
         try {
-            for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
                 if ("Nimbus".equals(info.getName())) {
-                    UIManager.setLookAndFeel(info.getClassName());
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
                     break;
                 }
             }
         } catch (ClassNotFoundException ex) {
-            Logger.getLogger(IncioDeSesion.class.getName()).log(Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(IncioDeSesion.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            Logger.getLogger(IncioDeSesion.class.getName()).log(Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(IncioDeSesion.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            Logger.getLogger(IncioDeSesion.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (UnsupportedLookAndFeelException ex) {
-            Logger.getLogger(IncioDeSesion.class.getName()).log(Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(IncioDeSesion.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(IncioDeSesion.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
-<<<<<<< HEAD
-        EventQueue.invokeLater(new Runnable() {
-=======
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
->>>>>>> parent of e8e0acb (feat: avance implementacion de firebase)
             public void run() {
                 new IncioDeSesion().setVisible(true);
             }
         });
     }
 
-<<<<<<< HEAD
-    private JPanel Background;
-    private JButton BotonIniciarSesion;
-    private JTextField CasillaContraseña;
-    private JTextField CasillaNombreUsuario;
-    private JLabel jLabel1;
-    private JLabel jLabel2;
-    private JLabel jLabel3;
-    private JLabel jLabel4;
-
-=======
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JPanel Background;
-    private javax.swing.JButton BotonIniciarSesion;
-    private javax.swing.JTextField CasillaContraseña;
-    private javax.swing.JTextField CasillaNombreUsuario;
+    private javax.swing.JTextField TextoContraseña;
+    private javax.swing.JTextField TextoUsuario;
+    private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     // End of variables declaration//GEN-END:variables
->>>>>>> parent of e8e0acb (feat: avance implementacion de firebase)
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,9 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-<<<<<<< HEAD
-=======
 
->>>>>>> parent of e8e0acb (feat: avance implementacion de firebase)
+
     }
-}
+    }
