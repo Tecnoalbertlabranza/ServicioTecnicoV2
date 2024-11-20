86734915655fb849201be14c3e45b3a254bd2126
Tecnoalbertlabranza <basty.wenckhansbello@gmail.com>
2024-11-19 21:40:55 -0300

    feat : Refresco de tabla despues de eliminar cliente
    ahora al momento de eliminar un cliente se actualiza instantaneamente la tabla de clientes

--- src/main/java/org/example/Interfaces/AdministracionClientes/EliminarCliente.java
+++ src/main/java/org/example/Interfaces/AdministracionClientes/EliminarCliente.java
@@ -54,7 +54,7 @@ public class EliminarCliente extends javax.swing.JPanel {
             e.printStackTrace();
             System.err.println("Error durante la operacion"+e.getMessage());
         }
-
+        clientes.refrescarTabla();
     }
 
     /**
