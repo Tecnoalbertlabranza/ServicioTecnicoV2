2d686ee9a02effd2f7a96787eb42dc68c6b62927
rz-eizy <duartealessandroxd@gmail.com>
2024-11-23 21:06:38 -0300

    feat: primer test
    Se creo un test que permite verificar si se puede encontrar a un cliente por su rut

--- src/test/java/org/example/ServicioTecnicoTest.java
+++ src/test/java/org/example/ServicioTecnicoTest.java
@@ -7,11 +7,27 @@ import java.util.List;
 import java.util.ArrayList;
 
 class ServicioTecnicoTest {
-    private ServicioTecnico clienteService;
+    private ServicioTecnico servicioTecnico;
     private List<Cliente> listaClientes;
 
     @BeforeEach
     void setUp() {
+        listaClientes = new ArrayList<>();
 
+        listaClientes.add(new Cliente("Juan", "Pérez", "123456789", "juan@example.com", "12345678-9", "Santiago", "Providencia"));
+        listaClientes.add(new Cliente("Ana", "González", "987654321", "ana@example.com", "98765432-1", "Valparaíso", "Viña del Mar"));
+
+        servicioTecnico = new ServicioTecnico("TecnoAlbert Labranza", null, null, null, listaClientes, "Descripción de servicio");
+    }
+
+    @Test
+    public void testObtenerDatosClientes() {
+        String rut = "12345678-9";
+
+        Cliente cliente = servicioTecnico.obtenerDatosDelCliente(rut);
+
+        assertNotNull(cliente);
+        assertEquals(rut, cliente.getRut());
+        assertEquals("Juan", cliente.getNombre());
     }
 }
