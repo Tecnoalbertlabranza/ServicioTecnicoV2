6d6b21414ec0f71965bc0caf11b42e6cf5e209d9
joxquinnn <joxquinnn1137@gmail.com>
2024-11-29 14:58:51 -0300

    fix: eliminar servicio pc
    se modificó el nombre de la tabla ingresada en firebase para que el metodo funcione correctamente

--- src/main/java/org/example/Interfaces/AdministracionServiciosPc/EliminarServiciosPc.java
+++ src/main/java/org/example/Interfaces/AdministracionServiciosPc/EliminarServiciosPc.java
@@ -35,7 +35,7 @@ public class EliminarServiciosPc extends javax.swing.JPanel {
     public void eliminarServicioPcConNombre(String nombre){
         try {
             Firestore db = firebaseInstance.getFirestore();
-            CollectionReference collectionRef = db.collection("Registro de servicio de Pc");
+            CollectionReference collectionRef = db.collection("Registro de servicio computador");
             ApiFuture<QuerySnapshot> query = collectionRef.whereEqualTo("Nombre", nombre).get();
             QuerySnapshot querySnapshot = query.get();
             System.out.println("Consultando Servicios para Pc con nombre : " + nombre);
