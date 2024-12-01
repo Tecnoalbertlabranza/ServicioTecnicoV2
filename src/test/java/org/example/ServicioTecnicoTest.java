e1d1b9cfa44e89708acec4d424e303e53e4803f0
rz-eizy <duartealessandroxd@gmail.com>
2024-11-30 23:51:33 -0300

    fix modificar y agregar test
    Se modificaron los test y se agrego uno para poder comprobar la eficiencia del codigo

--- src/test/java/org/example/ServicioTecnicoTest.java
+++ src/test/java/org/example/ServicioTecnicoTest.java
@@ -1,6 +1,8 @@
 package org.example;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import java.util.List;
@@ -14,20 +16,32 @@ class ServicioTecnicoTest {
     void setUp() {
         listaClientes = new ArrayList<>();
 
-        listaClientes.add(new Cliente("Juan", "Pérez", "123456789", "juan@example.com", "12345678-9", "Santiago", "Providencia"));
-        listaClientes.add(new Cliente("Ana", "González", "987654321", "ana@example.com", "98765432-1", "Valparaíso", "Viña del Mar"));
+        listaClientes.add(new Cliente("Juan", "Pérez", "123456789", "juan@example.com", "123456789", "Santiago", "Providencia"));
+        listaClientes.add(new Cliente("Ana", "González", "987654321", "ana@example.com", "987654321", "Valparaíso", "Viña del Mar"));
 
         servicioTecnico = new ServicioTecnico("TecnoAlbert Labranza", null, null, null, listaClientes, "Descripción de servicio");
     }
 
+    @AfterEach
+    void tearDown() {
+        servicioTecnico = null;
+    }
+
     @Test
     public void testObtenerDatosClientes() {
-        String rut = "12345678-9";
+        String rut = "123456789";
 
         Cliente cliente = servicioTecnico.obtenerDatosDelCliente(rut);
 
         assertNotNull(cliente);
         assertEquals(rut, cliente.getRut());
         assertEquals("Juan", cliente.getNombre());
+    }
+
+    @Test
+    public void testObtenerDatosClienteInvalido() {
+        String rut = "133456789";
+        Cliente cliente = servicioTecnico.obtenerDatosDelCliente(rut);
+        assertNull(cliente);
     }
 }
