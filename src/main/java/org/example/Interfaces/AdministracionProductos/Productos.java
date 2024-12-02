0c629d85b349433896abe08f0acdbf98fd24fe54
Tecnoalbertlabranza <basty.wenckhansbello@gmail.com>
2024-12-02 14:56:01 -0300

    correcion de nombres y limpieza de codigo

--- src/main/java/org/example/Interfaces/AdministracionProductos/Productos.java
+++ src/main/java/org/example/Interfaces/AdministracionProductos/Productos.java
@@ -164,7 +164,7 @@ public class Productos extends javax.swing.JPanel {
                 {null, null, null, null}
             },
             new String [] {
-                "Title 1", "Title 2", "Title 3", "Title 4"
+                "Nombre", "Categoria", "Valor", "Stock"
             }
         ));
         jScrollPane1.setViewportView(TablaProductos);
